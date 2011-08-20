b3978abfe909ab1f555298a1cfe3b78cc4a5e9e0
Cyril Poon <cyril.poon@china-rewards.com>
2011-08-20 13:54:50 +0800

    Added new project for QQ API.

---
+++ qqapi/src/main/java/com/chinarewards/qqgbvpn/App.java
@@ -0,0 +1,13 @@
+package com.chinarewards.qqgbvpn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
