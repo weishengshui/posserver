48634e9962b8ba86bc598ac9ef7a66883b026a99
wangmaolin <iori.wang@china-rewards.com>
2011-08-20 18:33:41 +0800

    update MD5 function,add throws Exception

--- qqapi/src/main/java/com/chinarewards/qqgbvpn/qqapi/util/TuanUtil.java
+++ qqapi/src/main/java/com/chinarewards/qqgbvpn/qqapi/util/TuanUtil.java
@@ -34,24 +34,21 @@ public class TuanUtil {
 	 * @author iori
 	 * @param s
 	 * @return
+	 * @throws Exception 
 	 */
-	public static String MD5(String s) {
-		try {
-			MessageDigest mdInst = MessageDigest.getInstance("MD5");
-			mdInst.update(s.getBytes());
-			byte[] md = mdInst.digest();
-			StringBuffer sb = new StringBuffer();
-			for (int i = 0; i < md.length; i++) {
-				int val = ((int) md[i]) & 0xff;
-				if (val < 16) {
-					sb.append("0");
-				}
-				sb.append(Integer.toHexString(val));
+	public static String MD5(String s) throws Exception {
+		MessageDigest mdInst = MessageDigest.getInstance("MD5");
+		mdInst.update(s.getBytes());
+		byte[] md = mdInst.digest();
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < md.length; i++) {
+			int val = ((int) md[i]) & 0xff;
+			if (val < 16) {
+				sb.append("0");
 			}
-			return sb.toString();
-		} catch (Exception e) {
-			return "";
+			sb.append(Integer.toHexString(val));
 		}
+		return sb.toString();
 	}
 	
 	/**
