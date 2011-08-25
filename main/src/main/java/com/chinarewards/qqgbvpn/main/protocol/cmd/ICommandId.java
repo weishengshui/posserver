3843d5459a5015cd8b5cd4edc4b76a0452d596f9
Cyril Poon <cyril.poon@china-rewards.com>
2011-08-25 16:11:05 +0800

    Added ICommandId interface.

---
+++ main/src/main/java/com/chinarewards/qqgbvpn/main/protocol/cmd/ICommandId.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.chinarewards.qqgbvpn.main.protocol.cmd;
+
+/**
+ * Defines the interface of an object which provides information about the
+ * command ID.
+ * 
+ * @author Cyril
+ * @since 0.1.0
+ */
+public interface ICommandId {
+
+	/**
+	 * Returns the command ID.
+	 * 
+	 * @return
+	 */
+	public long getCmdId();
+
+}
