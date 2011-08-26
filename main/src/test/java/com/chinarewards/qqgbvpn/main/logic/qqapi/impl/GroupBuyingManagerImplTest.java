76a0477a949128689e193050821369aa318d7d84
Cyril Poon <cyril.poon@china-rewards.com>
2011-08-26 16:53:57 +0800

    Added test skeleton for GroupBuygingManagerImpl.

---
+++ main/src/test/java/com/chinarewards/qqgbvpn/main/logic/qqapi/impl/GroupBuyingManagerImplTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.chinarewards.qqgbvpn.main.logic.qqapi.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.chinarewards.qqgpvn.main.test.GuiceTest;
+
+/**
+ * @author Cyril
+ *
+ */
+public class GroupBuyingManagerImplTest extends GuiceTest {
+
+	/* (non-Javadoc)
+	 * @see com.chinarewards.qqgpvn.main.test.GuiceTest#setUp()
+	 */
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
