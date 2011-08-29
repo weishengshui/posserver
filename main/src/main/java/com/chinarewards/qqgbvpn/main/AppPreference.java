8252b661d39fb695bbdad021ccee8fb20f3187cf
Cyril Poon <cyril.poon@china-rewards.com>
2011-08-29 18:33:27 +0800

    * Removed unused methods in AppPreference.

--- main/src/main/java/com/chinarewards/qqgbvpn/main/AppPreference.java
+++ main/src/main/java/com/chinarewards/qqgbvpn/main/AppPreference.java
@@ -11,14 +11,6 @@ package com.chinarewards.qqgbvpn.main;
  */
 public class AppPreference {
 
-	private String weiboUsername;
-
-	private String weiboPassword;
-
-	private String sinaProvinceId;
-
-	private String sinaCityId;
-
 	private String dbType;
 
 	private String db;
@@ -26,123 +18,6 @@ public class AppPreference {
 	private String dbUsername;
 
 	private String dbPassword;
-
-	private String[] microblogComments;
-
-	private Integer replyPerLoop;
-
-	private Integer pausePerReply;
-
-	private String commentFile;
-
-	/**
-	 * @return pause-per-reply. If this value is <code>null</code>, no limit
-	 *         will be applied.
-	 */
-	public Integer getPausePerReply() {
-		return pausePerReply;
-	}
-
-	/**
-	 * @param pausePerReply
-	 *            the pausePerReply to set
-	 */
-	public void setPausePerReply(Integer pausePerReply) {
-		this.pausePerReply = pausePerReply;
-	}
-
-	/**
-	 * Returns reply per loop. If this value is <code>null</code>, no limit will
-	 * be applied.
-	 * 
-	 * @return
-	 */
-	public Integer getReplyPerLoop() {
-		return replyPerLoop;
-	}
-
-	/**
-	 * @param replyPerLoop
-	 *            replyPerLoop to set
-	 */
-	public void setReplyPerLoop(Integer replyPerLoop) {
-		this.replyPerLoop = replyPerLoop;
-	}
-
-	/**
-	 * Returns the text used for replying microblog messages.
-	 */
-	public String[] getMicroblogComments() {
-		return microblogComments;
-	}
-
-	/**
-	 * @param microblogComments
-	 *            the microblogComments to set
-	 */
-	public void setMicroblogComments(String[] microblogComments) {
-		this.microblogComments = microblogComments;
-	}
-
-	/**
-	 * @return the weiboUsername
-	 */
-	public String getWeiboUsername() {
-		return weiboUsername;
-	}
-
-	/**
-	 * @param weiboUsername
-	 *            the weiboUsername to set
-	 */
-	public void setWeiboUsername(String weiboUsername) {
-		this.weiboUsername = weiboUsername;
-	}
-
-	/**
-	 * @return the weiboPassword
-	 */
-	public String getWeiboPassword() {
-		return weiboPassword;
-	}
-
-	/**
-	 * @param weiboPassword
-	 *            the weiboPassword to set
-	 */
-	public void setWeiboPassword(String weiboPassword) {
-		this.weiboPassword = weiboPassword;
-	}
-
-	/**
-	 * @return the sinaProvinceId
-	 */
-	public String getSinaProvinceId() {
-		return sinaProvinceId;
-	}
-
-	/**
-	 * @param sinaProvinceId
-	 *            the sinaProvinceId to set
-	 */
-	public void setSinaProvinceId(String sinaProvinceId) {
-		this.sinaProvinceId = sinaProvinceId;
-	}
-
-	/**
-	 * @return the sinaCityId
-	 */
-	public String getSinaCityId() {
-		return sinaCityId;
-	}
-
-	/**
-	 * @param sinaCityId
-	 *            the sinaCityId to set
-	 */
-	public void setSinaCityId(String sinaCityId) {
-		this.sinaCityId = sinaCityId;
-	}
 
 	/**
 	 * @return the dbType
@@ -204,21 +79,6 @@ public class AppPreference {
 	 */
 	public void setDb(String db) {
 		this.db = db;
-	}
-
-	/**
-	 * @return the commentFile
-	 */
-	public String getCommentFile() {
-		return commentFile;
-	}
-
-	/**
-	 * @param commentFile
-	 *            the commentFile to set
-	 */
-	public void setCommentFile(String commentFile) {
-		this.commentFile = commentFile;
 	}
 
 }
---
+++ main/src/main/java/com/chinarewards/qqgbvpn/main/HomeDirLocator.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package com.chinarewards.qqgbvpn.main;
+
+/**
+ * 
+ * 
+ * @author Cyril
+ * @since 0.1.0
+ */
+public class HomeDirLocator {
+
+	String hardCodedDir;
+	
+	public HomeDirLocator(String hardCodedDir) {
+		this.hardCodedDir = hardCodedDir;
+	}
+
+	public String getHomeDir() {
+
+		String r = hardCodedDir;
+		if (r != null)
+			return r;
+
+		// look at environment variable
+		r = this.getHomeDirFromSysEnv();
+		if (r != null)
+			return r;
+
+		// look at the executing directory
+		// TODO
+
+		return r;
+
+	}
+
+	/**
+	 * Returns the home directory based on the environment variable settings.
+	 * The key of the environment variable is based on the method
+	 * {@link #getHomeDirEnvName()}.
+	 * 
+	 * @return the home directory as found in the system environment.
+	 *         <code>nulL</code> if not found.
+	 */
+	protected String getHomeDirFromSysEnv() {
+
+		String v = System.getProperties().getProperty(getHomeDirEnvName());
+
+		if (v == null) {
+			// fallback
+			v = System.getenv(getHomeDirEnvName());
+		}
+
+		return v;
+
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getHomeDirEnvName() {
+		return "POSNET";
+	}
+
+}
