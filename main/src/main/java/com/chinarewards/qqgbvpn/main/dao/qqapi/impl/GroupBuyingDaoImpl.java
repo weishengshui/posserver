9e13b5273152df2ae33af325e5b44c7607ca46c4
wangmaolin <iori.wang@china-rewards.com>
2011-08-31 14:59:41 +0800

    modify qqapi unbind

--- main/src/main/java/com/chinarewards/qqgbvpn/main/dao/qqapi/impl/GroupBuyingDaoImpl.java
+++ main/src/main/java/com/chinarewards/qqgbvpn/main/dao/qqapi/impl/GroupBuyingDaoImpl.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.persistence.Query;
 
 import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.lang.StringUtils;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.ObjectMapper;
 
@@ -26,6 +26,7 @@ import com.chinarewards.qqgbvpn.main.dao.qqapi.GroupBuyingDao;
 import com.chinarewards.qqgbvpn.main.exception.CopyPropertiesException;
 import com.chinarewards.qqgbvpn.main.exception.SaveDBException;
 import com.chinarewards.qqgbvpn.qqapi.vo.GroupBuyingSearchListVO;
+import com.chinarewards.qqgbvpn.qqapi.vo.GroupBuyingUnbindVO;
 
 public class GroupBuyingDaoImpl extends BaseDaoImpl implements GroupBuyingDao {
 
@@ -242,52 +243,118 @@ public class GroupBuyingDaoImpl extends BaseDaoImpl implements GroupBuyingDao {
 	public void handleGroupBuyingUnbind(HashMap<String, Object> params) throws SaveDBException, JsonGenerationException {
 		String[] posIds = (String[]) params.get("posId");
 		String resultCode = (String) params.get("resultCode");
-		for (String posId : posIds) {
-			PosAssignment pa = getPosAssignmentByIdPosId(posId);
-			if (pa != null) {
-				Journal journal = new Journal();
-				journal.setTs(new Date());
-				journal.setEntity(DomainEntity.UNBIND_POS_ASSIGNMENT.toString());
-				journal.setEntityId(pa.getId());
-				journal.setEvent("0".equals(resultCode) ? DomainEvent.POS_UNBIND_SUCCESS.toString() : DomainEvent.POS_UNBIND_FAILED.toString());
-				ObjectMapper mapper = new ObjectMapper();
-				if ("0".equals(resultCode) && params.get("items") != null) {
-					try {
-						journal.setEventDetail(mapper.writeValueAsString(pa));
-					} catch (Exception e) {
-						throw new JsonGenerationException(e);
+		List<GroupBuyingUnbindVO> items = (List<GroupBuyingUnbindVO>) params.get("items");
+		Date data = new Date();
+		String resultStatus = "0";
+		//响应成功
+		if ("0".equals(resultCode)) {
+			for (String posId : posIds) {
+				resultStatus = this.getResultStatusByPosIdForUnbind(items, posId);
+				//resultStatus取消状态，非0代表成功
+				if (!"0".equals(resultStatus)) {
+					PosAssignment pa = getPosAssignmentByIdPosId(posId);
+					if (pa != null) {
+						Journal journal = new Journal();
+						journal.setTs(data);
+						journal.setEntity(DomainEntity.UNBIND_POS_ASSIGNMENT.toString());
+						journal.setEntityId(pa.getId());
+						journal.setEvent(DomainEvent.POS_UNBIND_SUCCESS.toString());
+						ObjectMapper mapper = new ObjectMapper();
+						if (items != null) {
+							try {
+								journal.setEventDetail(mapper.writeValueAsString(pa));
+							} catch (Exception e) {
+								throw new JsonGenerationException(e);
+							}
+						}
+						try {
+							if (!em.get().getTransaction().isActive()) {
+								em.get().getTransaction().begin();
+							}
+							if ("0".equals(resultCode)) {
+								em.get().remove(pa);
+							}
+							saveJournal(journal);
+							if (em.get().getTransaction().isActive()) {
+								em.get().getTransaction().commit();
+							}
+						} catch (Exception e) {
+							if (em.get().getTransaction().isActive()) {
+								em.get().getTransaction().rollback();
+							}
+							log.error("group buying unbind save error");
+							log.error("posId: " + posId);
+							log.error("ts: " + journal.getTs());
+							log.error("entity: " + journal.getEntity());
+							log.error("entityId: " + journal.getEntityId());
+							log.error("event: " + journal.getEvent());
+							log.error("eventDetail: " + journal.getEventDetail());
+							throw new SaveDBException(e);
+						}
+					} else {
+						log.error("group buying unbind get pos assignment error");
+						log.error("pos assignment not found by posId : " + posId);
+						throw new SaveDBException("group buying unbind error,pos assignment not found by posId : " + posId);
 					}
 				} else {
-					journal.setEventDetail(resultCode);
+					//resultStatus取消状态，0代表不成功，直接写失败日志
+					Journal journal = new Journal();
+					journal.setTs(data);
+					journal.setEntity(DomainEntity.UNBIND_POS_ASSIGNMENT.toString());
+					journal.setEntityId(posId);
+					journal.setEvent(DomainEvent.POS_UNBIND_FAILED.toString());
+					journal.setEventDetail(resultStatus);
+					try {
+						if (!em.get().getTransaction().isActive()) {
+							em.get().getTransaction().begin();
+						}
+						saveJournal(journal);
+						if (em.get().getTransaction().isActive()) {
+							em.get().getTransaction().commit();
+						}
+					} catch (Exception e) {
+						if (em.get().getTransaction().isActive()) {
+							em.get().getTransaction().rollback();
+						}
+						log.error("group buying unbind save error");
+						log.error("posId: " + posId);
+						log.error("ts: " + journal.getTs());
+						log.error("entity: " + journal.getEntity());
+						log.error("entityId: " + journal.getEntityId());
+						log.error("event: " + journal.getEvent());
+						log.error("eventDetail: " + journal.getEventDetail());
+						throw new SaveDBException(e);
+					}
 				}
-				try {
-					if (!em.get().getTransaction().isActive()) {
-						em.get().getTransaction().begin();
-					}
-					if ("0".equals(resultCode)) {
-						em.get().remove(pa);
-					}
-					saveJournal(journal);
-					if (em.get().getTransaction().isActive()) {
-						em.get().getTransaction().commit();
-					}
-				} catch (Exception e) {
-					if (em.get().getTransaction().isActive()) {
-						em.get().getTransaction().rollback();
-					}
-					log.error("group buying unbind save error");
-					log.error("posId: " + posId);
-					log.error("ts: " + journal.getTs());
-					log.error("entity: " + journal.getEntity());
-					log.error("entityId: " + journal.getEntityId());
-					log.error("event: " + journal.getEvent());
-					log.error("eventDetail: " + journal.getEventDetail());
-					throw new SaveDBException(e);
+			}
+		} else {
+			//resultCode不等于0说明QQ响应失败，直接写错误日志
+			Journal journal = new Journal();
+			journal.setTs(data);
+			journal.setEntity(DomainEntity.UNBIND_POS_ASSIGNMENT.toString());
+			journal.setEntityId(posIds.toString());
+			journal.setEvent(DomainEvent.POS_UNBIND_FAILED.toString());
+			journal.setEventDetail(resultCode);
+			try {
+				if (!em.get().getTransaction().isActive()) {
+					em.get().getTransaction().begin();
 				}
-			} else {
-				log.error("group buying unbind get pos assignment error");
-				log.error("pos assignment not found by posId : " + posId);
-				throw new SaveDBException("group buying unbind error,pos assignment not found by posId : " + posId);
+				saveJournal(journal);
+				if (em.get().getTransaction().isActive()) {
+					em.get().getTransaction().commit();
+				}
+			} catch (Exception e) {
+				if (em.get().getTransaction().isActive()) {
+					em.get().getTransaction().rollback();
+				}
+				log.error("group buying unbind save error");
+				log.error("posIds: " + StringUtils.join(posIds, ","));
+				log.error("ts: " + journal.getTs());
+				log.error("entity: " + journal.getEntity());
+				log.error("entityId: " + journal.getEntityId());
+				log.error("event: " + journal.getEvent());
+				log.error("eventDetail: " + journal.getEventDetail());
+				throw new SaveDBException(e);
 			}
 		}
 	}
@@ -376,6 +443,19 @@ public class GroupBuyingDaoImpl extends BaseDaoImpl implements GroupBuyingDao {
 			}
 		}
 		return resultCode;
+	}
+	
+	private String getResultStatusByPosIdForUnbind(List<GroupBuyingUnbindVO> items, String posId) {
+		String resultStatus = "0";
+		if (posId != null && items != null && items.size() > 0) {
+			for (GroupBuyingUnbindVO item : items) {
+				if (posId.equals(item.getPosId())) {
+					resultStatus = item.getResultStatus();
+					break;
+				}
+			}
+		}
+		return resultStatus;
 	}
 	
 }
