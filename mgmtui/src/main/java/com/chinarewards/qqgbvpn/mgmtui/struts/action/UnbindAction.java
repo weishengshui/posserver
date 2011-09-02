2413fd1c7b094a9e2d60dd67d2dbc9ac7fe0b3ab
wangmaolin <iori.wang@china-rewards.com>
2011-09-02 18:18:43 +0800

    add sendURL.jsp

--- mgmtui/src/main/java/com/chinarewards/qqgbvpn/mgmtui/struts/action/UnbindAction.java
+++ mgmtui/src/main/java/com/chinarewards/qqgbvpn/mgmtui/struts/action/UnbindAction.java
@@ -1,9 +1,10 @@
 package com.chinarewards.qqgbvpn.mgmtui.struts.action;
 
-import com.chinarewards.qqgbvpn.mgmtui.logic.GroupBuyingUnbindManager;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.chinarewards.qqgbvpn.domain.Agent;
 import com.chinarewards.qqgbvpn.mgmtui.struts.BaseAction;
-import com.google.inject.Inject;
-import com.google.inject.Provider;
 
 /**
  * pos unbind action
@@ -18,6 +19,25 @@ public class UnbindAction extends BaseAction {
 	//@Inject
 	//private Provider<GroupBuyingUnbindManager> groupBuyingUnbindMgr;
 
+	String agentName;
+	List<Agent> agentList;
+	
+	public List<Agent> getAgentList() {
+		return agentList;
+	}
+
+	public void setAgentList(List<Agent> agentList) {
+		this.agentList = agentList;
+	}
+
+	public String getAgentName() {
+		return agentName;
+	}
+
+	public void setAgentName(String agentName) {
+		this.agentName = agentName;
+	}
+
 	@Override
 	public String execute(){
 		return SUCCESS;
@@ -28,6 +48,18 @@ public class UnbindAction extends BaseAction {
 	}
 	
 	public String sendURL() {
+		System.out.println(agentName);
+		if ("a1".equals(agentName)) {
+			agentList = new ArrayList<Agent>();
+			Agent a1 = new Agent();
+			a1.setId("id1");
+			a1.setName("a1");
+			Agent a2 = new Agent();
+			a2.setId("id2");
+			a2.setName("a2");
+			agentList.add(a1);
+			agentList.add(a2);
+		}
 		return SUCCESS;
 	}
 
