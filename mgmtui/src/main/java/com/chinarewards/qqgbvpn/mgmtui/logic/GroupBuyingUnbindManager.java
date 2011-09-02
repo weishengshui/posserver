ffcb0ce6382263893619de8f30b8b46818d7b647
wangmaolin <iori.wang@china-rewards.com>
2011-09-02 15:40:17 +0800

    add unbind service interface

--- mgmtui/src/main/java/com/chinarewards/qqgbvpn/mgmtui/logic/GroupBuyingUnbindManager.java
+++ mgmtui/src/main/java/com/chinarewards/qqgbvpn/mgmtui/logic/GroupBuyingUnbindManager.java
@@ -1,9 +1,14 @@
 package com.chinarewards.qqgbvpn.mgmtui.logic;
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.codehaus.jackson.JsonGenerationException;
 
+import com.chinarewards.qqgbvpn.domain.Agent;
+import com.chinarewards.qqgbvpn.domain.PageInfo;
+import com.chinarewards.qqgbvpn.domain.Pos;
+import com.chinarewards.qqgbvpn.domain.ReturnNote;
 import com.chinarewards.qqgbvpn.mgmtui.exception.SaveDBException;
 import com.chinarewards.qqgbvpn.qqapi.exception.MD5Exception;
 import com.chinarewards.qqgbvpn.qqapi.exception.ParseXMLException;
@@ -20,4 +25,46 @@ public interface GroupBuyingUnbindManager {
 	 */
 	public HashMap<String, Object> groupBuyingUnbind(
 			HashMap<String, Object> params) throws MD5Exception, ParseXMLException, SendPostTimeOutException, JsonGenerationException, SaveDBException;
+	
+	/**
+	 * 根据第三方ID分页查询POS机列表
+	 * @param pageInfo
+	 * @param agentId
+	 * @return
+	 */
+	public PageInfo getPosByAgentId(PageInfo pageInfo, String agentId);
+	
+	/**
+	 * 根据第三方名称查询第三方
+	 * @param agentName
+	 * @return
+	 */
+	public Agent getAgentByName(String agentName);
+	
+	/**
+	 * 根据posId、sn、simPhoneNo查询相应POS机
+	 * @param info
+	 * @return
+	 */
+	public List<Pos> getPosByPosInfo(String info);
+	
+	/**
+	 * 根据第三方ID生成空回收单
+	 * @param agentId
+	 * @return
+	 * @throws JsonGenerationException
+	 * @throws SaveDBException
+	 */
+	public ReturnNote createReturnNoteByAgentId(String agentId) throws JsonGenerationException,SaveDBException;
+	
+	/**
+	 * 确认回收单
+	 * @param agentId
+	 * @param rnId
+	 * @param posList
+	 * @return
+	 * @throws JsonGenerationException
+	 * @throws SaveDBException
+	 */
+	public ReturnNote confirmReturnNote(String agentId,String rnId,List<Pos> posList) throws JsonGenerationException,SaveDBException;
 }
