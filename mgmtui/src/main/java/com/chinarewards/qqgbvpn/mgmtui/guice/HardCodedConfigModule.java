8727b7b2df937cc7d319fda0894217887b25b44d
wangmaolin <iori.wang@china-rewards.com>
2011-09-05 15:17:28 +0800

    add posnet.ini

---
+++ mgmtui/src/main/java/com/chinarewards/qqgbvpn/mgmtui/guice/HardCodedConfigModule.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package com.chinarewards.qqgbvpn.mgmtui.guice;
+
+import java.io.InputStream;
+import java.util.Iterator;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * FIXME this is for temporary solution only.
+ * 
+ * @author cyril
+ * @since 0.1.0
+ */
+public class HardCodedConfigModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+
+		Configuration configuration = null;
+		try {
+			configuration = readConfigFromResource();
+			debugPrintConfiguration(configuration);
+		} catch (ConfigurationException e) {
+			System.err.println("Error loading configuration from resource");
+			e.printStackTrace();
+		}
+		// and bind it
+		bind(Configuration.class).toInstance(configuration);
+
+	}
+
+	protected Configuration readConfigFromResource()
+			throws ConfigurationException {
+
+		PropertiesConfiguration configuration = new PropertiesConfiguration();
+
+		InputStream is = Thread.currentThread().getContextClassLoader()
+				.getResourceAsStream("posnet.ini");
+		configuration.load(is);
+		try {
+			is.close();
+		} catch (Throwable t) {
+			// mute it.
+		}
+
+		return configuration;
+	}
+
+	protected void debugPrintConfiguration(Configuration conf) {
+		Iterator iter = conf.getKeys();
+		System.out.println("Configuration listing:");
+		while (iter.hasNext()) {
+			String key = (String)iter.next();
+			System.out.println("- " + key + ": " + conf.getString(key));
+		}
+	}
+
+}
