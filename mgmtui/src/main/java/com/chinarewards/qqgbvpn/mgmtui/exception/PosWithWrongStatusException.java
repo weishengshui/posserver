0361af08830caa1d326ad0ee6311840d561602c4
seek <seek.yao@china-rewards.com>
2011-09-08 10:55:22 +0800

    commmit code

---
+++ mgmtui/src/main/java/com/chinarewards/qqgbvpn/mgmtui/exception/PosWithWrongStatusException.java
@@ -0,0 +1,37 @@
+/**
+ * description：
+ * @copyright binfen.cc
+ * @projectName mgmtui
+ * @time 2011-9-7   下午06:28:20
+ * @author Seek
+ */
+package com.chinarewards.qqgbvpn.mgmtui.exception;
+
+/**
+ * description：Pos with wrong status.
+ * 
+ * @copyright binfen.cc
+ * @projectName mgmtui
+ * @time 2011-9-7 下午06:28:20
+ * @author Seek
+ */
+public class PosWithWrongStatusException extends Exception {
+
+	private static final long serialVersionUID = -3821601313180634376L;
+
+	public PosWithWrongStatusException() {
+	}
+
+	public PosWithWrongStatusException(String message) {
+		super(message);
+	}
+
+	public PosWithWrongStatusException(Throwable cause) {
+		super(cause);
+	}
+
+	public PosWithWrongStatusException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
