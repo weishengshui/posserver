16edee9b70d5d79441e466c0a63a9c0982876b98
cream <cream.zhan@china-rewards.com>
2011-09-09 10:54:39 +0800

    Add delivery search API.

---
+++ mgmtui/src/main/java/com/chinarewards/qqgbvpn/mgmtui/model/delivery/DeliverySearchVO.java
@@ -0,0 +1,96 @@
+package com.chinarewards.qqgbvpn.mgmtui.model.delivery;
+
+import java.util.Date;
+
+import com.chinarewards.qqgbvpn.mgmtui.model.util.PaginationTools;
+
+public class DeliverySearchVO {
+
+	String id;
+
+	String dnNumber;
+
+	String status;
+
+	Date createDate;
+	Date confirmDate;
+	Date printDate;
+
+	String agentName;
+
+	PaginationTools pagination;
+
+	@Override
+	public String toString() {
+		return "DeliverySearchVO [id=" + id + ", dnNumber=" + dnNumber
+				+ ", status=" + status + ", createDate=" + createDate
+				+ ", confirmDate=" + confirmDate + ", printDate=" + printDate
+				+ ", agentName=" + agentName + ", pagination=" + pagination
+				+ "]";
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDnNumber() {
+		return dnNumber;
+	}
+
+	public void setDnNumber(String dnNumber) {
+		this.dnNumber = dnNumber;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Date getConfirmDate() {
+		return confirmDate;
+	}
+
+	public void setConfirmDate(Date confirmDate) {
+		this.confirmDate = confirmDate;
+	}
+
+	public Date getPrintDate() {
+		return printDate;
+	}
+
+	public void setPrintDate(Date printDate) {
+		this.printDate = printDate;
+	}
+
+	public String getAgentName() {
+		return agentName;
+	}
+
+	public void setAgentName(String agentName) {
+		this.agentName = agentName;
+	}
+
+	public PaginationTools getPagination() {
+		return pagination;
+	}
+
+	public void setPagination(PaginationTools pagination) {
+		this.pagination = pagination;
+	}
+
+}
