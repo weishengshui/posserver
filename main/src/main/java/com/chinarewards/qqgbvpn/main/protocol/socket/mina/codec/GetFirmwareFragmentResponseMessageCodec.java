158da7bec670932b884649850b3438db69bf638e
Cyril Poon <cyril.poon@china-rewards.com>
2011-09-15 11:14:40 +0800

    * Added one codec implementation.

---
+++ main/src/main/java/com/chinarewards/qqgbvpn/main/protocol/socket/mina/codec/GetFirmwareFragmentResponseMessageCodec.java
@@ -0,0 +1,63 @@
+package com.chinarewards.qqgbvpn.main.protocol.socket.mina.codec;
+
+import java.nio.charset.Charset;
+
+import org.apache.mina.core.buffer.IoBuffer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.chinarewards.qqgbvpn.main.exception.PackageException;
+import com.chinarewards.qqgbvpn.main.protocol.cmd.GetFirmwareFragmentResponseMessage;
+import com.chinarewards.qqgbvpn.main.protocol.cmd.ICommand;
+import com.chinarewards.qqgbvpn.main.protocol.socket.ProtocolLengths;
+
+/**
+ * Implements the codec for the message get firmware fragment response.
+ * <p>
+ * XXX add test case for this codec
+ * 
+ * @author cyril
+ * @since 0.1.0
+ */
+public class GetFirmwareFragmentResponseMessageCodec implements ICommandCodec {
+
+	// private Logger log = LoggerFactory.getLogger(getClass());
+
+	@Override
+	public ICommand decode(IoBuffer in, Charset charset)
+			throws PackageException {
+
+		throw new UnsupportedOperationException(
+				"Message decoding not supported");
+	}
+
+	/**
+	 * TODO implements encoding function.
+	 */
+	@Override
+	public byte[] encode(ICommand bodyMessage, Charset charset) {
+
+		GetFirmwareFragmentResponseMessage msg = (GetFirmwareFragmentResponseMessage) bodyMessage;
+
+		// prepare buffer
+		IoBuffer buf = IoBuffer.allocate(ProtocolLengths.COMMAND
+				+ ProtocolLengths.RESULT);
+		if (msg.getContent() != null && msg.getContent().length > 0) {
+			buf.expand(msg.getContent().length);
+		}
+		buf.position(0);
+
+		// encode data
+		// command ID
+		buf.putUnsignedInt(msg.getCmdId());
+		// result
+		buf.putUnsignedShort(msg.getResult());
+		// content - optional
+		if (msg.getContent() != null) {
+			buf.put(msg.getContent());
+		}
+
+		// return result
+		return buf.array();
+	}
+}
