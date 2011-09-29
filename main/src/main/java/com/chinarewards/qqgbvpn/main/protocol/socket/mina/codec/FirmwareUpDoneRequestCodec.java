a4cdc6b53dd908df4f660ecbb0c354f15e86a158
seek <seek.yao@china-rewards.com>
2011-09-29 13:04:30 +0800

    implements codec!

--- main/src/main/java/com/chinarewards/qqgbvpn/main/protocol/socket/mina/codec/FirmwareUpDoneRequestCodec.java
+++ main/src/main/java/com/chinarewards/qqgbvpn/main/protocol/socket/mina/codec/FirmwareUpDoneRequestCodec.java
@@ -1,13 +1,18 @@
 package com.chinarewards.qqgbvpn.main.protocol.socket.mina.codec;
 
 import java.nio.charset.Charset;
+import java.util.Arrays;
+
 import org.apache.mina.core.buffer.IoBuffer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.chinarewards.qqgbvpn.common.Tools;
 import com.chinarewards.qqgbvpn.main.exception.PackageException;
+import com.chinarewards.qqgbvpn.main.protocol.cmd.CmdConstant;
 import com.chinarewards.qqgbvpn.main.protocol.cmd.FirmwareUpDoneRequestMessage;
 import com.chinarewards.qqgbvpn.main.protocol.cmd.ICommand;
+import com.chinarewards.qqgbvpn.main.protocol.cmd.InitRequestMessage;
+import com.chinarewards.qqgbvpn.main.protocol.cmd.ValidateRequestMessage;
 import com.chinarewards.qqgbvpn.main.protocol.socket.ProtocolLengths;
 
 /**
@@ -45,10 +50,29 @@ public class FirmwareUpDoneRequestCodec implements ICommandCodec {
 		log.debug("FirmwareUpDone message request:cmdId is ({}) , posid is ({}) ",new Object[]{cmdId,posIdBuf});
 		return message;
 	}
-
+	
+	/**
+	 * description：mock pos test use it!
+	 * @param bodyMessage
+	 * @param charset
+	 * @return
+	 * @time 2011-9-29   下午12:56:18
+	 * @author Seek
+	 */
 	@Override
 	public byte[] encode(ICommand bodyMessage, Charset charset) {
-		return null;
+		//...
+		log.debug("FirmwareUpDone message encode");
+		FirmwareUpDoneRequestMessage requestMessage = (FirmwareUpDoneRequestMessage) bodyMessage;
+		long cmdId = requestMessage.getCmdId();
+		String posId = requestMessage.getPosId();
+
+		byte[] resultByte = new byte[ProtocolLengths.COMMAND + ProtocolLengths.POS_ID];
+		
+		Tools.putUnsignedInt(resultByte, cmdId, 0);
+		Tools.putBytes(resultByte, posId.getBytes(), ProtocolLengths.COMMAND);
+		
+		return resultByte;
 	}
 
 }
