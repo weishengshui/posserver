9fc19db2d2f4db709bfe5ccc4424a3f904fa5d26
wangmaolin <iori.wang@china-rewards.com>
2011-10-24 09:58:24 +0800

    modify table name to upper case

--- main/src/db/patches/000004_finance_account.sql
+++ main/src/db/patches/000004_finance_account.sql
@@ -1,1 +1,1 @@
-INSERT INTO sysuser (username, password) VALUES ('finance','57336afd1f4b40dfd9f5731e35302fe5');
+INSERT INTO SysUser (username, PASSWORD) VALUES ('finance','57336afd1f4b40dfd9f5731e35302fe5');
